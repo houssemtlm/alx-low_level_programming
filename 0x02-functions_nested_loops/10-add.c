69f91693381587d4ecd654196f18c1222eefdeaa
Houssem <houssemtllemsani@gmail.com>
2023-03-22 19:41:48 -0700

    the sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers and return the result
+ * @n1: first input
+ * @n2: second input
+ * Return: Always 0 (Success)
+*/
+int add(int n1, int n2)
+{
+	int result = n1 + n2;
+
+	return (result);
+}
