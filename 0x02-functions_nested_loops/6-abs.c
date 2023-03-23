d6d03a0e0a2af2f06d78dae4a3107cc6011a086e
Houssem <houssemtllemsani@gmail.com>
2023-03-22 19:04:39 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - return the absolute value of n
+ * @n: checks input
+ * Return: Always 0 (Success)
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n
+	return (0);
+}
