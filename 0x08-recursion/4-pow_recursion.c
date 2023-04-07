38a157c0696de3b5801cc70bd36b90382b0dd1ae
Houssem <houssemtllemsani@gmail.com>
2023-04-06 19:16:44 -0700

    value of x raised to the value of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,8 @@
  * _pow_recursion - returns @x raised to the power of @y
  * @x: first input
  * @y: second input
- * Return: -1 if y is lower than 0 and 1 if y is 0 and x if y is 1 else x to the power of y if
+ * Return: -1 if y is lower than 0 and 1 if y is 0 and
+ *         x if y is 1 else x to the power of y if
 */
 int _pow_recursion(int x, int y)
 {
@@ -13,5 +14,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	else if (y == 1)
 		return (x);
-	return(x * _pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
